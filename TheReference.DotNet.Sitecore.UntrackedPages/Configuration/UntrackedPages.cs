f82c9e06b78b1d03de8f3dc9190bac1b7dd5159c
jvantroyen <jvantroyen@reference.be>
2017-08-17 16:51:10 +0200

    Add project files.

---
+++ TheReference.DotNet.Sitecore.UntrackedPages/Configuration/UntrackedPages.cs
@@ -0,0 +1,63 @@
+﻿using Sitecore.Analytics.Configuration;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TheReference.DotNet.Sitecore.UntrackedPages.Configuration
+{
+    public class UntrackedPageList
+    {
+        private List<string> untrackedPages;
+
+        public UntrackedPageList(List<string> untrackedPages)
+        {
+            this.untrackedPages = untrackedPages;
+        }
+
+        public static UntrackedPageList GetUntrackedPages()
+        {
+            return new UntrackedPageList(GetUntrackedPageList());
+        }
+
+        public bool ContainsUntrackedPage(string page)
+        {
+            Assert.ArgumentNotNull(page, "page");
+            return untrackedPages.Any(untrackedPage => new WildcardPattern(untrackedPage).IsMatch(page));
+        }
+
+        private static List<string> GetUntrackedPageList()
+        {
+            List<string> list = new List<string>();
+            XmlNode configNode = Factory.GetConfigNode("tracking/untrackedPages");
+            if (configNode != null)
+            {
+                foreach (XmlNode node in configNode.ChildNodes)
+                {
+                    if (node.Name == "add")
+                    {
+                        string str = XmlUtil.GetAttribute("path", node, true);
+                        if (str.Contains("?"))
+                        {
+                            Log.Warn("Query strings are not supported for pages configured under tracking/untrackedPages. Query string was removed from: " + str, (object)typeof(AnalyticsSettings));
+                            str = str.Remove(str.IndexOf("?", StringComparison.Ordinal));
+                        }
+                        if (!str.StartsWith("/"))
+                        {
+                            Log.Warn("Page path must begin with '/' for pages configured under tracking/untrackedPages. '/' was added to: " + str, (object)typeof(AnalyticsSettings));
+                            str = "/" + str;
+                        }
+                        list.Add(str);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ TheReference.DotNet.Sitecore.UntrackedPages/Pipelines/CheckPreconditions.cs
@@ -0,0 +1,79 @@
+﻿using Sitecore;
+using Sitecore.Analytics;
+using Sitecore.Analytics.Configuration;
+using Sitecore.Analytics.Tracking.Diagnostics.PerformanceCounters;
+using Sitecore.Diagnostics;
+using Sitecore.Layouts;
+using Sitecore.Pipelines;
+using Sitecore.Sites;
+using System;
+using TheReference.DotNet.Sitecore.UntrackedPages.Configuration;
+
+namespace TheReference.DotNet.Sitecore.UntrackedPages.Pipelines
+{
+    public class CheckPreconditions
+    {
+        private static object excludeListSync = new object();
+        private static UntrackedPageList excludeList;
+
+        public static UntrackedPageList ExcludeList
+        {
+            get
+            {
+                if (excludeList == null)
+                {
+                    lock (excludeListSync)
+                    {
+                        if (excludeList == null)
+                            excludeList = UntrackedPageList.GetUntrackedPages();
+                    }
+                }
+                return excludeList;
+            }
+            internal set
+            {
+                excludeList = value;
+            }
+        }
+
+        public virtual void Process(PipelineArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, "args");
+            if (!Tracker.Enabled)
+            {
+                args.AbortPipeline();
+            }
+            else
+            {
+                SiteContext site = Context.Site;
+                if (site == null)
+                {
+                    args.AbortPipeline();
+                }
+                else
+                {
+                    PageContext page = Context.Page;
+                    if (page == null)
+                        args.AbortPipeline();
+                    else if (!TrackingSiteContextExtension.Tracking(site).EnableTracking)
+                        args.AbortPipeline();
+                    else if (this.IsUntrackedPage(page.FilePath))
+                        args.AbortPipeline();
+                    else
+                        AnalyticsTrackingCount.CollectionTotalRequests.Increment(1L);
+                }
+            }
+        }
+
+        protected bool IsUntrackedPage(string filePath)
+        {
+            Assert.ArgumentNotNull((object)filePath, "filePath");
+            if (ExcludeList.ContainsUntrackedPage(filePath))
+            {
+                Log.Warn("UntrackedPage - " + filePath, this);
+                return true;
+            }
+            return false;
+        }
+    }
+}
