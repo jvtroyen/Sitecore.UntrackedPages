03c8e61c2f6591b5658c7aafc7fd3333dd05b5d5
jvantroyen <jvantroyen@reference.be>
2017-08-17 17:01:32 +0200

    Initial version

--- TheReference.DotNet.Sitecore.UntrackedPages/Configuration/UntrackedPages.cs
+++ TheReference.DotNet.Sitecore.UntrackedPages/Configuration/UntrackedPages.cs
@@ -6,8 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace TheReference.DotNet.Sitecore.UntrackedPages.Configuration
